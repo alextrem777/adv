a0526b89207cd7201adc83590cded59d2fc9a5f6
alextrem777 <73223975+alextrem777@users.noreply.github.com>
2020-12-08 17:24:04 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,28 @@ $(function(){
 	$('.order-call').on('click', function(){
 		$('.popup-container').css('display', 'flex');
 	});
+
+	$('.close').on('click', function(){
+		$('.popup-container').css('display', 'none');
+	});
+
+	$('.send').on('click', function(){
+		let request = {
+			'name': $('#name').val(),
+			'email': $('#email').val()
+
+		};
+		$.post('/api.php', request, function(response){
+			if (response=='1'){
+				$('#email').val(''),
+				$('#name').val(''),
+				$('.popup-container').css('display', 'none');
+			}
+
+
+		})
+		
+	});
 	
 
 });
